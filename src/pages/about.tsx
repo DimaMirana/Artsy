049f3f29528df65acb7fdfea7abda6182ed51089
TasnimDima <codemen.dima@gmail.com>
2022-11-03 09:10:47 +0600

    add about section

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -14,7 +14,8 @@ const About = () => {
                     {t('About')}
                 </Typography>
                 <div>
-                    information abou company will go here
+                    <h3>information about company:</h3>
+                    <p>The product wind is a wallet app that is build on blockchain. This walled can be used to transactions between people that are connected in this app with nominal fees. Wind works on the end-to-end transaction via chain varification (block chain algorithm) and in this way there is no third paty involved. This makes transactions instant and without giving extra additional charge to any other 3rd party provider. This app use the currency name stablecoins that can be later transferred to usd via KYC check. As it's a non-custodial wallet, only the owner thas the access to their money and thus only the sole owner has the access to thier asset. To earn on this app one can work as a mine worker This app can be found on Play store or App Store.</p>
                 </div>
             </Box>
         </div>
